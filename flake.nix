9d4c946574609b2c3b80582a822269883c9be3ec
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-04-15 20:31:16 -0400

    Pass files to hooks

--- flake.nix
+++ flake.nix
@@ -32,7 +32,7 @@
 
           text = ''
             # Note that we use the pnpm version of eslint, which supports plugins
-            pnpm --dir "$(git rev-parse --show-toplevel)"/frontend exec eslint . --fix
+            pnpm --dir "$(git rev-parse --show-toplevel)"/frontend exec eslint . --fix "''${@#frontend/}" # Remove the "frontend/" prefix from filenames before passing to prettier, since prettier is being run in the /frontend directory anyways
           '';
         };
 
@@ -42,7 +42,7 @@
 
           text = ''
             # Note that we use the pnpm version of eslint, which supports plugins
-            pnpm --dir "$(git rev-parse --show-toplevel)"/frontend exec prettier --write .
+            pnpm --dir "$(git rev-parse --show-toplevel)"/frontend exec prettier --write "''${@#frontend/}"
           '';
         };
       in {

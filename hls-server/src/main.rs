b8507b980f6bf97aee5a917a1b2b90f9fdf4b920
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-04-17 10:40:49 -0400

    feature: create basic axum HTTP server

--- hls-server/src/main.rs
+++ hls-server/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+
+const PORT: i16 = 3000;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello world" }));
+
+    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{PORT}"))
+        .await
+        .unwrap();
+
+    println!("Listening on {PORT}...");
+    axum::serve(listener, app).await.unwrap();
 }

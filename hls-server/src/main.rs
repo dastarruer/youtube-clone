cd33d1910c253e29487525afa11942b982a65bff
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-04-21 18:02:42 -0400

    WIP: Implement ffmpeg

--- hls-server/src/main.rs
+++ hls-server/src/main.rs
@@ -1,10 +1,10 @@
-use axum::{Router, routing::get};
+use axum::{Router, extract::Path, response::IntoResponse, routing::get};
 
 const PORT: i16 = 3000;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello world" }));
+    let app = Router::new().route("/", get(root));
 
     let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{PORT}"))
         .await
@@ -12,4 +12,13 @@ async fn main() {
 
     println!("Listening on {PORT}...");
     axum::serve(listener, app).await.unwrap();
+}
+
+async fn root() -> impl IntoResponse {
+    let source = Path("./videos/test.mp4");
+    let destination = Path("./videos/converted.mp4");
+    
+    ffmpeg_next::init().expect(msg)
+
+    "TODO"
 }
